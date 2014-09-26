9e7ca009431839ba20c207e7edfa975e7a4a8c85
LaserMedia <system@plugscripts.com>
2014-09-26 12:50:43 -0500

    AJAX Request Added
    Adds message to the end of the room's welcome message to let people know
    that it is running.

--- block-soundcloud.js
+++ block-soundcloud.js
@@ -1,9 +1,34 @@
-$(window).unload(function(){
-	API.sendChat("/em SoundCloudSkip Disabled.");
-})
-
-API.sendChat("/em After the current song finishes, SoundCloud songs will automatically be skipped.");
-API.on(API.ADVANCE,mediaChecker);
+if (typeof originalMessage==='undefined'){
+	$(window).unload(function(){
+		$.ajax({
+			type: 'POST',
+			url: '/_/rooms/update',
+			contentType: 'application/json',
+			data: JSON.stringify({
+				"welcome":originalMessage
+			})
+		});
+	})
+	var originalMessage=$($(".welcome").children()[2]).text();
+	var welcome=$($(".welcome").children()[2]).text();
+	if (welcome=="No welcome message"){
+		originalMessage="";
+		welcome="SoundCloudSkip Active.";
+	}
+	else{
+		welcome+=" SoundCloudSkip Active.";
+	}
+	$.ajax({
+		type: 'POST',
+		url: '/_/rooms/update',
+		contentType: 'application/json',
+		data: JSON.stringify({    
+			"welcome":welcome
+		})
+	});
+	API.sendChat("/em After the current song finishes, SoundCloud songs will automatically be skipped.");
+	API.on(API.ADVANCE,mediaChecker);
+}
 function mediaChecker(){
 	if(API.getMedia().format===2){
 		API.moderateForceSkip();
--- block-youtube.js
+++ block-youtube.js
@@ -1,9 +1,34 @@
-$(window).unload(function(){
-	API.sendChat("/em YouTubeSkip Disabled.");
-})
-
-API.sendChat("/em After the current song finishes, YouTube songs will automatically be skipped.");
-API.on(API.ADVANCE,mediaChecker);
+if (typeof originalMessage==='undefined'){
+	$(window).unload(function(){
+		$.ajax({
+			type: 'POST',
+			url: '/_/rooms/update',
+			contentType: 'application/json',
+			data: JSON.stringify({
+				"welcome":originalMessage
+			})
+		});
+	})
+	var originalMessage=$($(".welcome").children()[2]).text();
+	var welcome=$($(".welcome").children()[2]).text();
+	if (welcome=="No welcome message"){
+		originalMessage="";
+		welcome="YouTubeSkip Active.";
+	}
+	else{
+		welcome+=" YouTubeSkip Active.";
+	}
+	$.ajax({
+		type: 'POST',
+		url: '/_/rooms/update',
+		contentType: 'application/json',
+		data: JSON.stringify({    
+			"welcome":welcome
+		})
+	});
+	API.sendChat("/em After the current song finishes, YouTube songs will automatically be skipped.");
+	API.on(API.ADVANCE,mediaChecker);
+}
 function mediaChecker(){
 	if(API.getMedia().format===1){
 		API.moderateForceSkip();
